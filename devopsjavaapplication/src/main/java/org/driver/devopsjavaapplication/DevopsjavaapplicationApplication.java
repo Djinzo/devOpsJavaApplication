3f366ef8a0239959136865b0a2a31b53691e0bbc
Amine-G <mgoulzima@externe.gbp.ma>
2024-07-20 22:30:59 +0100

    first commit

---
+++ devopsjavaapplication/src/main/java/org/driver/devopsjavaapplication/DevopsjavaapplicationApplication.java
@@ -0,0 +1,13 @@
+package org.driver.devopsjavaapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsjavaapplicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsjavaapplicationApplication.class, args);
+    }
+
+}
---
+++ devopsjavaapplication/src/test/java/org/driver/devopsjavaapplication/DevopsjavaapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.driver.devopsjavaapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsjavaapplicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
